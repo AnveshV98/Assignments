89209d3eb7cb60f6651f2cf66a14270c129978fe
Anvesh Veldhandi <88618849+AnveshV98@users.noreply.github.com>
2021-10-27 21:57:59 +0530

    Add files via upload

---
+++ spring-security-rolebased/src/main/java/com/yash/RoleBasedSecurity/SpringSecurityRolebasedApplication.java
@@ -0,0 +1,13 @@
+package com.yash.RoleBasedSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityRolebasedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityRolebasedApplication.class, args);
+	}
+
+}
